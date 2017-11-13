f54263bc60198d50319a9b6e5f3c61a636d74804
Andrew Schultz <abschultz20@gmail.com>
2017-11-13 14:34:00 -0500

    Authentication with Auth0

---
+++ serverless/auth-service/handler.js
@@ -0,0 +1,74 @@
+'use strict';
+
+const jwk = require('jsonwebtoken');
+const jwkToPem = require('jwk-to-pem');
+const request = require('request');
+
+const iss = 'https://mi-app.auth0.com/';
+
+// Generate policy to allow this user on this API:
+const generatePolicy = (principalId, effect, resource) => {
+  const authResponse = {};
+  authResponse.principalId = principalId;
+  if (effect && resource) {
+    const policyDocument = {};
+    policyDocument.Version = '2012-10-17';
+    policyDocument.Statement = [];
+    const statementOne = {};
+    statementOne.Action = 'execute-api:Invoke';
+    statementOne.Effect = effect;
+    statementOne.Resource = resource;
+    policyDocument.Statement[0] = statementOne;
+    authResponse.policyDocument = policyDocument;
+  }
+  return authResponse;
+};
+
+module.exports.auth = (event, context, cb) => {
+  console.log('Auth function invoked');
+  if (event.authorizationToken) {
+    // Remove 'bearer ' from token:
+    const token = event.authorizationToken.substring(7);
+    // Make a request to the iss + .well-known/jwks.json URL:
+    request(
+      { url: `${iss}.well-known/jwks.json`, json: true },
+      (error, response, body) => {
+        if (error || response.statusCode !== 200) {
+          console.log('Request error:', error);
+          cb('Unauthorized');
+        }
+        const keys = body;
+        // Based on the JSON of `jwks` create a Pem:
+        const k = keys.keys[0];
+        const jwkArray = {
+          kty: k.kty,
+          n: k.n,
+          e: k.e,
+        };
+        const pem = jwkToPem(jwkArray);
+
+        // Verify the token:
+        jwk.verify(token, pem, { issuer: iss }, (err, decoded) => {
+          if (err) {
+            console.log('Unauthorized user:', err.message);
+            cb('Unauthorized');
+          } else {
+            cb(null, generatePolicy(decoded.sub, 'Allow', event.methodArn));
+          }
+        });
+      });
+  } else {
+    console.log('No authorizationToken found in the header.');
+    cb('Unauthorized');
+  }
+};
+
+// Public API
+module.exports.publicEndpoint = (event, context, cb) => {
+  cb(null, { message: 'Welcome to our Public API!' });
+};
+
+// Private API
+module.exports.privateEndpoint = (event, context, cb) => {
+  cb(null, { message: 'Only logged in users can see this' });
+};
